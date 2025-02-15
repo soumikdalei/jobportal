74889558f455e00476f05a40f5f6f64b972ab6d7
soumikdalei <soumikdalei@gmail.com>
2025-02-15 07:26:20 +0530

    Some changes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from './controllers/webhook.js'
+import companyRoutes from './routes/companyRoutes.js'
 const app=express()
 await connectDB()
 app.use(cors())
@@ -16,6 +17,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
 app.post('/webHooks',clerkWebhooks)
+app.use('/api/company',companyRoutes)
 const PORT =process.env.PORT || 5000
 Sentry.setupExpressErrorHandler(app);
 app.listen(PORT,()=>{
